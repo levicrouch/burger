5173ad792c51a316a21448f3a2fbdf254d6f7202
Levi Crouch <levi.crouch@gmail.com>
2018-01-16 14:23:40 -0800

    seeding code from cats app

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+// MySQL Connection file
+
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+
+// Variables for hosting on Heroku
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "dbuser",
+        password: "SecureSQLPw%1",
+        database: "burgers_db"
+    });
+}
+
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Plain Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon & Bleu Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Burger');
